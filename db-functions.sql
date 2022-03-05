f94ad7ead180342d9911c9a9918b38fd78d60ef2
Azlain Saavedra <azlain.saavedra@gmail.com>
2022-03-05 14:59:52 -0500

    add db fucntion

---
+++ db-functions.sql
@@ -0,0 +1,8 @@
+CREATE DEFINER=`root`@`%` FUNCTION `wf_workflow`.`SPLIT_STR`(
+  x VARCHAR(255),
+  delim VARCHAR(12),
+  pos INT
+) RETURNS varchar(255) CHARSET latin1
+RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(x, delim, pos),
+       LENGTH(SUBSTRING_INDEX(x, delim, pos -1)) + 1),
+       delim, '');
